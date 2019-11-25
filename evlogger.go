dfcf8aed0d9b5e2568723e1cc1f3112870b46dea
Hayden Maclean <haydensmaclean@gmail.com>
2019-11-25 00:30:18 -0500

    set up to loop five times printing the output of the cmd. Started a parse function pattern matching the "sh cells" call. g1 - cell number, g2 - voltage

--- evlogger.go
+++ evlogger.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"github.com/spf13/viper"
 	"flag"
 	"fmt"
 	"os"
 	"time"
 
-	"github.com/tarm/serial"
+	"github.com/spf13/viper"
+
 	"github.com/golang/glog"
+	"github.com/tarm/serial"
+	//"github.com/golang/regex"
 )
 
 // Version holds the version and build commit hash, set at build time.
@@ -16,25 +18,39 @@ var Version string
 
 func main() {
 	VersionFlag := flag.Bool("version", false, "prints commit hash for current build")
+	Cmd := flag.String("cmd", "help", "talk serial")
 	flag.Parse()
 
-	// TODO: set up glog with logDir value from the config 
+	// TODO: set up glog with logDir value from the config
 
 	if *VersionFlag {
-		fmt.Printf("%s\n", Version)
+		fmt.Printf("%s", Version)
 		os.Exit(0)
 	}
 
-	c := &serial.Config{Name: viper.GetString("serialBMSPath"), Baud: viper.GetInt("serialBaud"), ReadTimeout: time.Millisecond * 500}
-	s, err := serial.OpenPort(c)
-	if err != nil {
-		glog.Fatal(err)
-	}
+	if *Cmd != "" {
 
-	out, err := Execute(s, "sh h")
-	if err != nil {
-		glog.Error(err)
+		c := &serial.Config{Name: viper.GetString("serialEVCCPath"), Baud: viper.GetInt("serialBaud"), ReadTimeout: time.Millisecond * 500}
+		s, err := serial.OpenPort(c)
+		if err != nil {
+			glog.Fatal(err)
+		}
+
+		for i := 0; i < 5; i++ {
+
+			out, err := Execute(s, *Cmd)
+			if err != nil {
+				glog.Error(err)
+			}
+
+			fmt.Printf("%s", out)
+		}
 	}
+}
+
+func (o *string) Parse() (string, error) {
+	reCells := regex.MustCompile(`(c\d{1,2}).{3}(\d.\d{3})`)
+
 }
 
 // Execute takes a pointer to a serial.Port and command string to execute, returns string and error
@@ -45,12 +61,13 @@ func Execute(s *serial.Port, cmd string) (string, error) {
 		return "", err
 	}
 
-	buf := make([]byte, 128)
+	buf := make([]byte, 4096)
 	output := ""
 
 	for {
+		// HACK: used magic number to detect end of output ("evcc> \n" > 8 bytes?)
 		n, err = s.Read(buf)
-		if n == 0 {
+		if n == 0 || n == 8 {
 			break
 		}
 
