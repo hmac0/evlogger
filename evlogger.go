2e61d2a6ce82f0ae195bd597a16607d726b72d34
hmac0 <kc1ipm@gmail.com>
2022-01-01 22:38:25 -0500

    code cleanup, whitespace

--- evlogger.go
+++ evlogger.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"regexp"
 	"time"
 
 	"github.com/spf13/viper"
@@ -23,15 +22,12 @@ func main() {
 	SerialDevice := flag.String("serial_device", "/dev/ttyUSB0", "the name of the tty device you want to talk to")
 	flag.Parse()
 
-	// TODO: set up glog with logDir value from the config
-
 	if *VersionFlag {
 		fmt.Printf("%s", Version)
 		os.Exit(0)
 	}
 
 	if *Cmd != "" {
-
 		c := &serial.Config{Name: *SerialDevice, Baud: viper.GetInt("serialBaud"), ReadTimeout: time.Millisecond * 50}
 		s, err := serial.OpenPort(c)
 		if err != nil {
@@ -41,63 +37,35 @@ func main() {
 		out, err := Execute(s, *Cmd)
 		if err != nil {
 			glog.Error(err)
+		} else {
+			glog.Infof("%s", out)
 		}
-
-		glog.V(2).Infof("%s", out)
-
-		// paresedOutput := Parse(out, `(c\d{1,2}).{3}(\d.\d{3})`, "bms,cell=%s volts=%s\n")
-		// if err != nil {
-		// 	glog.Error(err)
-
-		// }
-		// glog.V(2).Infof("%s", paresedOutput)
 	}
-}
-
-// Parse
-func Parse(input string, regex string, outputPattern string) string {
-	re := regexp.MustCompile(regex)
-
-	// `(c\d{1,2}).{3}(\d.\d{3})`\
-	r := re.FindAllString(input, -1)
-
-	glog.V(2).Infof("%v", r)
-
-	parsedOutput := ""
-	for _, elem := range r {
-		parsedOutput += fmt.Sprintf(outputPattern, elem[1:])
-	}
-	return parsedOutput
-
 }
 
 // Execute takes a pointer to a serial.Port and command string to execute, returns string and error
 func Execute(s *serial.Port, cmd string) (string, error) {
+
+	// record start time for performance monitoring
 	start := time.Now()
+
+	// write command as a byte array into serial port
 	_, err := s.Write([]byte(fmt.Sprintf("%s\n", cmd)))
 	if err != nil {
 		glog.Error(err)
 		return "", err
 	}
 
+	// TODO: should the buffer be created ahead of sending the command
+	// TODO: break up sending the command and receiving the output
+
+	// init new buffer for serial output
+	// NOTE: '\x3E' is a magic char, that should indicate the end of the output
 	reader := bufio.NewReader(s)
 	buf, err := reader.ReadBytes('\x3E')
 	if err != nil {
-		panic(err)
+		glog.Error(err)
 	}
-	//fmt.Println(reply)
-
-	// buf := make([]byte, 1024)
-	// output := ""
-
-	// for {
-	// 	n, _ = s.Read(buf)
-	// 	if n == 0 {
-	// 		break
-	// 	}
-	// 	glog.Infof("%+v", buf[:n])
-	// 	output = output + string(buf[:n])
-	// }
 
 	glog.V(2).Infof("execute took %d ms", (time.Since(start)).Nanoseconds()/1000000)
 
